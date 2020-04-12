ba3089da752dfc631ae52fc2eb3dd9aa3cdbf8fe
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-04-12 21:35:45 +0700

    refactor

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import { Server } from 'http'
 import express from 'express'
-import socketio from 'socket.io'
+import useSocket from './useSocket'
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 const server = Server(app)
-const io = socketio(server)
+useSocket(server)
 
 server.listen(PORT, () => {
   console.info(`ready on port ${PORT}`)
@@ -15,26 +15,5 @@ server.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.json({
     serverStatus: 'online',
-  })
-})
-
-const ioCache = {
-  messages: [],
-}
-
-io.on('connect', (socket) => {
-  console.log(`socket ${socket.id} has connected`)
-  io.emit('broadcast', ioCache.messages)
-
-  socket.on('cc', (payload) => {
-    const cloned = payload
-    cloned.chatid = ioCache.messages.length
-    ioCache.messages = [cloned, ...ioCache.messages]
-    io.emit('broadcast', ioCache.messages)
-  })
-
-  socket.on('disconnect', (reason) => {
-    console.info(`socket ${socket.id} has disconnected`)
-    console.info(`reason : ${reason}`)
   })
 })
